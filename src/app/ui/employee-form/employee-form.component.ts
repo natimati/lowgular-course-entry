bd1ab8174eb11360ed05ce4ce07560b5e2082012
natimati <natmateusiak@gmail.com>
2022-11-16 17:07:24 +0100

    feat: after submit click displaying alert with form values

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -9,8 +9,12 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup({
-    name: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required]),
     age: new FormControl(null, [Validators.min(0)]),
     salary: new FormControl(null,[Validators.min(0), Validators.required])
   });
+
+  onButtonClick(form: { email: string, age: number, salary: number }) {
+    alert("user successfully added to the database. Email: " + form.email + ", Age: " + form.age + ", Salary: " + form.salary)
+  }
 }
