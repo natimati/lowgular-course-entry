e36a7983d992a61431d7f72136489c0016930d5f
natimati <natmateusiak@gmail.com>
2022-11-22 19:13:37 +0100

    feat: changing endpoint and map data

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) { }
 
   getAll(): Observable<PersonModel[] | null> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees'
+      ).pipe(map((response: ApiResponse<EmployeeResponse[]>) => {
+        return response.data.map((employeeResponse: EmployeeResponse ) => {
+          return {
+            name: employeeResponse.employee_name,
+            personalNumber: employeeResponse.id,
+            img: employeeResponse.profile_image,
+            surname: '',
+            mail: '',
+          }
+        })
+    }))
   }
   create(employee: CreateEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/create', employee)
